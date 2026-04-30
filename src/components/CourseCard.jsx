100caf791536ddae2064e9ab48d53cfc3c689026
Badsha-Nishan <badshanisan14@gmail.com>
2026-04-30 14:25:17 +0600

    added popular section

--- src/components/CourseCard.jsx
+++ src/components/CourseCard.jsx
@@ -1,63 +1,19 @@
-import { Button, Card, Chip, Separator } from "@heroui/react";
-import Image from "next/image";
-import Link from "next/link";
-import { FaDownload, FaHeart } from "react-icons/fa";
+import DetailsCard from "./DetailsCard";
 
 const CourseCard = async () => {
   const res = await fetch("https://skillsphere-hub.vercel.app/data.json");
   const courses = await res.json();
-  const {
-    id,
-    title,
-    image,
-    instructor,
-    duration,
-    rating,
-    level,
-    description,
-    category,
-  } = courses;
+  const popularCourse = courses.sort((a, b) => b.rating - a.rating).slice(0, 3);
+
   return (
-    <Card className="border my-4">
-      <div className="space-y-4">
-        <div className="w-full relative aspect-square">
-          <Image
-            className="rounded-md object-cover"
-            src={image}
-            fill
-            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-            alt={title}
-          ></Image>
-          <Chip size="sm" className="absolute top-2 right-2">
-            {category}
-          </Chip>
-        </div>
-        <div>
-          <h2 className="font-semibold text-xl">{title}</h2>
-        </div>
-        <div className="flex gap-5">
-          <div className="flex items-center gap-2">
-            <p className="text-red-500">
-              {" "}
-              <FaHeart />
-            </p>
-            <p>{rating}</p>
-          </div>
-          <Separator orientation="vertical" />
-          <div className="flex items-center gap-2">
-            <p className="text-green-500">
-              <FaDownload />
-            </p>
-            <p>{duration}</p>
-          </div>
-        </div>
-        <Link href={`/all-photos/${id}`}>
-          <Button variant="secondary" className={"w-full"}>
-            View Course
-          </Button>
-        </Link>
+    <div>
+      <p className="text-xl font-semibold">Popular Courses</p>
+      <div className="grid gap-4 grid-cols-3">
+        {popularCourse.map((course) => (
+          <DetailsCard key={course.id} course={course} />
+        ))}
       </div>
-    </Card>
+    </div>
   );
 };
 
---
+++ src/components/DetailsCard.jsx
@@ -0,0 +1,62 @@
+import { Button, Card, Chip, Separator } from "@heroui/react";
+import Image from "next/image";
+import Link from "next/link";
+import { FaDownload, FaHeart, FaStar } from "react-icons/fa";
+
+const DetailsCard = ({ course }) => {
+  const {
+    id,
+    title,
+    image,
+    instructor,
+    duration,
+    rating,
+    level,
+    description,
+    category,
+  } = course;
+  return (
+    <div>
+      <Card className="border my-4">
+        <div className="space-y-4">
+          <div className="w-full relative aspect-square">
+            <Image
+              className="rounded-md object-cover"
+              src={image}
+              fill
+              sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+              alt={title}
+            ></Image>
+            <Chip size="sm" className="absolute top-2 right-2">
+              {category}
+            </Chip>
+          </div>
+          <div>
+            <h2 className="font-semibold text-xl">{title}</h2>
+          </div>
+          <div className="flex gap-5">
+            <div className="flex items-center gap-2">
+              <p className="font-semibold">Instructor:</p>
+              <p>{instructor}</p>
+            </div>
+            <Separator orientation="vertical" />
+            <div className="flex items-center gap-2">
+              <p className="font-semibold">Rating:</p>
+              <p className="text-yellow-500">
+                <FaStar />
+              </p>
+              <p>{rating}</p>
+            </div>
+          </div>
+          <Link href={`/courses/${id}`}>
+            <Button variant="secondary" className={"w-full"}>
+              View Details
+            </Button>
+          </Link>
+        </div>
+      </Card>
+    </div>
+  );
+};
+
+export default DetailsCard;
